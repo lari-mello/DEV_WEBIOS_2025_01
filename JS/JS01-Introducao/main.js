3f73a5b7fe6ab60f7d63891e1eefa37f0fd4110d
larissa-mello <lari_mello.s@outlook.com>
2025-06-03 21:39:50 -0300

    Introdução JS - Parte 1

---
+++ JS/JS01-Introducao/main.js
@@ -0,0 +1,33 @@
+var idade
+const ano = 2010
+//Java Script
+console.log("Hello World") //Apresentar uma mensagem
+console.clear() //Serve para limpar uma tela de saida
+
+
+//Variáveis
+var numero1 = 192
+console.log(numero1)
+
+var num1 = 4
+var num2 = 6
+var resultado = num1 + num2
+console.log(resultado)
+
+idade = 48 //Iniciar a variavel em outra parte do codigo
+console.log(idade)
+
+ano = 2036
+
+//Textos - String
+var nome = 'Matheus Araujo'
+var nome2 = "Matheus Vinicius"
+var nome3 = '123456' //Entra como texto pois está entre ''
+console.log(nome)
+console.log(nome2)
+console.log(nome3)
+// posso fazer tanto individualmente sendo um em cada linha quanto separando por virgula, por exemplo console.log(nome, nome2, nome3) a diferença é que dessa forma as informações ficarão lado a lado
+
+Var // Flexivel e com menos regras (Posso alterar o valor icicial de uma var e recriar a mesma em outras partes do código)
+Let // Você não pode recriar a mesma em outras partes do codigo com o mesmo nome, mas pode chamar e alterar o valor inicial)
+Const //Você não pode recriar a mesma em outras partes do codigo e não pode alterar o valor inicial dela garantindo a confiança do dado que está sendo utilizado no decorrer do seu codigo
