4d97280c86af82fea8327317754cbd32fb5d6f7c
larissa-mello <lari_mello.s@outlook.com>
2025-06-04 21:34:14 -0300

    Instrodução JS pt2

--- JS/JS01-Introducao/main.js
+++ JS/JS01-Introducao/main.js
@@ -1,5 +1,5 @@
 var idade
-const ano = 2010
+var ano = 2010
 //Java Script
 console.log("Hello World") //Apresentar uma mensagem
 console.clear() //Serve para limpar uma tela de saida
@@ -28,6 +28,42 @@ console.log(nome2)
 console.log(nome3)
 // posso fazer tanto individualmente sendo um em cada linha quanto separando por virgula, por exemplo console.log(nome, nome2, nome3) a diferença é que dessa forma as informações ficarão lado a lado
 
-Var // Flexivel e com menos regras (Posso alterar o valor icicial de uma var e recriar a mesma em outras partes do código)
-Let // Você não pode recriar a mesma em outras partes do codigo com o mesmo nome, mas pode chamar e alterar o valor inicial)
-Const //Você não pode recriar a mesma em outras partes do codigo e não pode alterar o valor inicial dela garantindo a confiança do dado que está sendo utilizado no decorrer do seu codigo
+//Var // Flexivel e com menos regras (Posso alterar o valor icicial de uma var e recriar a mesma em outras partes do código)
+//Let // Você não pode recriar a mesma em outras partes do codigo com o mesmo nome, mas pode chamar e alterar o valor inicial)
+//Const //Você não pode recriar a mesma em outras partes do codigo e não pode alterar o valor inicial dela garantindo a confiança do dado que está sendo utilizado no decorrer do seu codigo
+
+console.log("Olá " + "Papai") //Concatenar
+console.log("Aluno " + nome + " e Aluno " + nome2 + " Tem um total de " + nome3 + " dinheiros ") //Concatenar
+
+//Template String
+console.log(`Aluno ${nome} e aluno ${nome2} tem um total de ${nome3} dinheiros`)
+
+//Operadores
+var valor1 = 5
+var valor2 = 5
+console.log( valor1 +  valor2)
+console.log( valor1 -  valor2)
+console.log( valor1 *  valor2)
+console.log( valor1 /  valor2)
+console.log( valor1 **  valor2)
+
+//Operadores relacionais
+console.log(5 == 5)
+console.log(valor1 == valor2)
+console.log(5 > 5)
+console.log(22 < 8)
+console.log(22 >= 22)
+console.log(22 != 23)
+console.log('A' == 'A');
+console.log(true == true);  
+console.log(true === true);
+console.log(true === 'true'); //boolean e o outro txt > String
+console.log(true != 'false');
+
+//Operadores lógicos
+let temCamisa = false
+let temCracha = true
+console.log(temCamisa);
+console.log(temCracha);
+console.log(temCamisa == true && temCracha == true); 
+console.log(temCamisa == true || temCracha == true);
