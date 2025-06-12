97f9323487bde9f118afc8f4bdda83732de83722
larissa-mello <lari_mello.s@outlook.com>
2025-06-12 20:59:10 -0300

    Exercicio 4

---
+++ JS/JS04-Repeticao/JS04-ExercicioCasa/main.js
@@ -0,0 +1,10 @@
+var numeroTabuada = 2
+if(numeroTabuada == 2){
+for(i = 0; i <= 10; i++){
+console.log(`${numeroTabuada} x ${i} = ${numeroTabuada * i}`)
+}
+}else if(numeroTabuada == 3){
+console.log("okay");
+}else{
+    console.log("Numero incorreto")
+}
