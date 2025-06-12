137610e032863d2e7c018de6c5fc71aae645d4ba
larissa-mello <lari_mello.s@outlook.com>
2025-06-12 20:49:00 -0300

    Inicio aula repetição

---
+++ JS/JS04-Repeticao/main.js
@@ -0,0 +1,14 @@
+let contador = 0 //Inicialização
+//Enquanto a variavel contador for menor que 10 o while apresentará o valor no console
+while(contador <= 10 ){
+    console.log(contador)
+    //contador = contador + 1
+    //contador +=1
+    contador++
+}
+console.log("Finalizou programa")
+
+//variavel inicial; condição; incremento
+for(contador = 0;contador <= 10 ;contador++){
+console.log(contador)
+}
